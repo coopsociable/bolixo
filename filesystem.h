6f14e8a455e6afa6dfea16fd3b24bf4df198bcda
jack <jack@c3086684-d568-4a45-a57d-89166b08296d>
2018-02-20 23:49:28 +0000

    Work in progress to write to inboxes
    git-svn-id: https://solucorp.bolixo.org/repos/solucorp/bolixo/trunk@2521 c3086684-d568-4a45-a57d-89166b08296d

--- filesystem.h
+++ filesystem.h
@@ -18,6 +18,8 @@ struct ENTRY {
 	}
 };
 
+#define END_OF_TIME	"3000/01/01 00:00:00"
+
 int fs_findentry (const char *name, ENTRY &entry, bool expect_exist, const char *threshold);
 FILE *fs_alloc_file_handle (int fileid, PARAM_STRING modified, const char *mode, std::string &handle, const char *sessionid);
 long fs_get_filesize (int fileid, PARAM_STRING modified);
@@ -25,3 +27,8 @@ FILE *fs_get_file (const std::string &handle, const char *sessionid);
 void fs_delete_handle (PARAM_STRING handle);
 std::string fs_makeid (int noproc);
 unsigned fs_getnbhandle();
+void fs_list_inboxes (unsigned userid, std::vector<std::string> &managers, std::vector<std::string> &projects, std::vector<std::string> &roles);
+unsigned fs_find_inbox (unsigned ownerid, const char *name, int noproc, std::string &msg);
+void fs_set_now (char now[20]);
+int fs_newid (unsigned userid, int noproc, std::string &msg, std::string &uuid);
+int fs_newid (unsigned userid, int noproc, std::string &msg);
