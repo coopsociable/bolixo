f32051bf332c9b0b93ec859a66aeabf2cdb3a4c6
jack <jack@c3086684-d568-4a45-a57d-89166b08296d>
2018-03-12 00:28:18 +0000

    Missing file in svn
    git-svn-id: https://solucorp.bolixo.org/repos/solucorp/bolixo/trunk@2582 c3086684-d568-4a45-a57d-89166b08296d

---
+++ bolixo.h
@@ -0,0 +1,21 @@
+enum ENTRY_TYPE{
+	ENTRY_NONE,
+	ENTRY_DELETED,
+	ENTRY_DIR,
+	ENTRY_FILE,
+	ENTRY_MSG
+};
+
+inline bool bolixo_isfile (ENTRY_TYPE type)
+{
+	return type == ENTRY_FILE || type == ENTRY_MSG;
+}
+inline bool bolixo_isdir (ENTRY_TYPE type)
+{
+	return type == ENTRY_DIR;
+}
+inline bool bolixo_isdeleted(ENTRY_TYPE type)
+{
+	return type == ENTRY_DELETED;
+}
+
