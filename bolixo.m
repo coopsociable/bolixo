55df7c245405a115c98c687b9c8fd0d12e07701b
jack <jack@c3086684-d568-4a45-a57d-89166b08296d>
2018-09-29 20:28:40 +0000

    Add separate titles (from id) in webtabs
    git-svn-id: https://solucorp.bolixo.org/repos/solucorp/bolixo/trunk@2828 c3086684-d568-4a45-a57d-89166b08296d

--- bolixo.m
+++ bolixo.m
@@ -2,7 +2,7 @@ extern const char **_dictionary_bolixo;
 #ifndef DICTIONARY_REQUEST
 	#define DICTIONARY_REQUEST \
 	const char **_dictionary_bolixo;\
-	TRANSLATE_SYSTEM_REQ _dictionary_req_bolixo("bolixo",_dictionary_bolixo,168,3);\
+	TRANSLATE_SYSTEM_REQ _dictionary_req_bolixo("bolixo",_dictionary_bolixo,169,3);\
 	void dummy_dict_bolixo(){}
 #endif
 #ifndef MSG_U
@@ -181,3 +181,4 @@ extern const char **_dictionary_bolixo;
 #define I_WRITECONFIG	165
 #define I_HELLOCONTACT	166
 #define E_CANTUPDDATEDIR	167
+#define I_NEWDOC	168
