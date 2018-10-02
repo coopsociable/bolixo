8b9dd48290efb1c41f15374e3b74db99f4a31d06
jack <jack@c3086684-d568-4a45-a57d-89166b08296d>
2018-10-02 23:27:55 +0000

    Add presentation of messages and images on the right in short messages (talk)
    git-svn-id: https://solucorp.bolixo.org/repos/solucorp/bolixo/trunk@2840 c3086684-d568-4a45-a57d-89166b08296d

--- bolixo.m
+++ bolixo.m
@@ -2,7 +2,7 @@ extern const char **_dictionary_bolixo;
 #ifndef DICTIONARY_REQUEST
 	#define DICTIONARY_REQUEST \
 	const char **_dictionary_bolixo;\
-	TRANSLATE_SYSTEM_REQ _dictionary_req_bolixo("bolixo",_dictionary_bolixo,174,3);\
+	TRANSLATE_SYSTEM_REQ _dictionary_req_bolixo("bolixo",_dictionary_bolixo,176,3);\
 	void dummy_dict_bolixo(){}
 #endif
 #ifndef MSG_U
@@ -187,3 +187,5 @@ extern const char **_dictionary_bolixo;
 #define I_PROJECT	171
 #define I_HELP	172
 #define I_HELPTOPIC	173
+#define I_TALKGROUP	174
+#define I_TALKFROM	175
