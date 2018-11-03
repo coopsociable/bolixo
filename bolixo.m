2db534bb55816b1a03e4ee3a1bbc1252faacba51
jack <jack@c3086684-d568-4a45-a57d-89166b08296d>
2018-11-03 01:40:02 +0000

    Copie, paste, delete, undelete work for files and folders.
    git-svn-id: https://solucorp.bolixo.org/repos/solucorp/bolixo/trunk@2933 c3086684-d568-4a45-a57d-89166b08296d

--- bolixo.m
+++ bolixo.m
@@ -2,7 +2,7 @@ extern const char **_dictionary_bolixo;
 #ifndef DICTIONARY_REQUEST
 	#define DICTIONARY_REQUEST \
 	const char **_dictionary_bolixo;\
-	TRANSLATE_SYSTEM_REQ _dictionary_req_bolixo("bolixo",_dictionary_bolixo,239,6);\
+	TRANSLATE_SYSTEM_REQ _dictionary_req_bolixo("bolixo",_dictionary_bolixo,241,6);\
 	void dummy_dict_bolixo(){}
 #endif
 #ifndef MSG_U
@@ -252,3 +252,5 @@ extern const char **_dictionary_bolixo;
 #define E_NOTDIRUNDEL	236
 #define E_NODELETED	237
 #define M_RENAME	238
+#define E_NOTHINGTOPASTE	239
+#define E_CANTPASTEENTRY	240
