d14b7efe58882753c5e58c8d67ed6ffed2d25c27
jack <jack@c3086684-d568-4a45-a57d-89166b08296d>
2019-01-10 17:14:27 +0000

    bolixod only accepts https nodes in production
    git-svn-id: https://solucorp.bolixo.org/repos/solucorp/bolixo/trunk@3161 c3086684-d568-4a45-a57d-89166b08296d

--- bolixo.m
+++ bolixo.m
@@ -2,7 +2,7 @@ extern const char **_dictionary_bolixo;
 #ifndef DICTIONARY_REQUEST
 	#define DICTIONARY_REQUEST \
 	const char **_dictionary_bolixo;\
-	TRANSLATE_SYSTEM_REQ _dictionary_req_bolixo("bolixo",_dictionary_bolixo,389,9);\
+	TRANSLATE_SYSTEM_REQ _dictionary_req_bolixo("bolixo",_dictionary_bolixo,390,9);\
 	void dummy_dict_bolixo(){}
 #endif
 #ifndef MSG_U
@@ -402,3 +402,4 @@ extern const char **_dictionary_bolixo;
 #define E_NOGROUPLISTID	386
 #define E_DIR_SUBDIRS	387
 #define O_NONSTRICT	388
+#define O_DEVMODE	389
