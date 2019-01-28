74b251274c3d9957e4ff5379a6ea3a0ed1531f67
jack <jack@c3086684-d568-4a45-a57d-89166b08296d>
2019-01-28 03:50:56 +0000

    Talk section in mobile is now working nicely.
    git-svn-id: https://solucorp.bolixo.org/repos/solucorp/bolixo/trunk@3228 c3086684-d568-4a45-a57d-89166b08296d

--- bolixo.m
+++ bolixo.m
@@ -2,7 +2,7 @@ extern const char **_dictionary_bolixo;
 #ifndef DICTIONARY_REQUEST
 	#define DICTIONARY_REQUEST \
 	const char **_dictionary_bolixo;\
-	TRANSLATE_SYSTEM_REQ _dictionary_req_bolixo("bolixo",_dictionary_bolixo,403,11);\
+	TRANSLATE_SYSTEM_REQ _dictionary_req_bolixo("bolixo",_dictionary_bolixo,404,11);\
 	void dummy_dict_bolixo(){}
 #endif
 #ifndef MSG_U
@@ -416,3 +416,4 @@ extern const char **_dictionary_bolixo;
 #define O_SESSION	400
 #define E_STAT	401
 #define H_FILTER2	402
+#define I_WRITEMSG	403
