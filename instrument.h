3903ac5b5e5e111e8e4cb391c3c95ba78f525889
jack <jack@c3086684-d568-4a45-a57d-89166b08296d>
2019-02-22 04:27:33 +0000

    Rework of this small header
    git-svn-id: https://solucorp.bolixo.org/repos/solucorp/bolixo/trunk@3313 c3086684-d568-4a45-a57d-89166b08296d

--- instrument.h
+++ instrument.h
@@ -1,9 +1,9 @@
-#ifdef INSTRUMENT_DONOTOPEN
-	FILE *f_instrument = NULL;
-#else
-	#ifdef INSTRUMENT
-		FILE *f_instrument = fopen ("/tmp/instrument.log","a");
+#ifdef INSTRUMENT
+	#ifdef INSTRUMENT_DONOTOPEN
+		FILE *f_instrument = NULL;
 	#else
-		extern FILE *f_instrument;
+		FILE *f_instrument = fopen ("/tmp/instrument.log","a");
 	#endif
+#else
+	extern FILE *f_instrument;
 #endif
