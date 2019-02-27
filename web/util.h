9639bddfd08fb549781a2b0201d89b1abaed620a
jack <jack@c3086684-d568-4a45-a57d-89166b08296d>
2019-02-27 02:51:53 +0000

    Problem with auto-reload of the public page to get its geometry
    git-svn-id: https://solucorp.bolixo.org/repos/solucorp/bolixo/trunk@3321 c3086684-d568-4a45-a57d-89166b08296d

--- web/util.h
+++ web/util.h
@@ -136,5 +136,6 @@ class MESSAGE_receive;
 string index_format_mail_fname(const MESSAGE_receive &m, PARAM_STRING username);
 void util_delnotify(CONNECT_INFO &con_sess, PARAM_STRING id);
 void util_delnotify(CONNECT_INFO &con_sess, PARAM_STRING prefix, PARAM_STRING id);
+void util_endscript(int webstep, PARAM_STRING urlparam);
 void util_endscript(int webstep);
-void util_endscript();
+void util_endscript(PARAM_STRING urlparam);
