5c750338a245bdb4304517b803a6d7cd5e77a048
jack <jack@c3086684-d568-4a45-a57d-89166b08296d>
2019-03-12 14:34:42 +0000

    Translation in rssd, debugging
    git-svn-id: https://solucorp.bolixo.org/repos/solucorp/bolixo/trunk@3354 c3086684-d568-4a45-a57d-89166b08296d

--- bolixo.m
+++ bolixo.m
@@ -2,7 +2,7 @@ extern const char **_dictionary_bolixo;
 #ifndef DICTIONARY_REQUEST
 	#define DICTIONARY_REQUEST \
 	const char **_dictionary_bolixo;\
-	TRANSLATE_SYSTEM_REQ _dictionary_req_bolixo("bolixo",_dictionary_bolixo,476,15);\
+	TRANSLATE_SYSTEM_REQ _dictionary_req_bolixo("bolixo",_dictionary_bolixo,477,15);\
 	void dummy_dict_bolixo(){}
 #endif
 #ifndef MSG_U
@@ -489,3 +489,4 @@ extern const char **_dictionary_bolixo;
 #define O_MINIPHOTO	473
 #define I_NOTE	474
 #define I_ONLYWHENLOGGED	475
+#define I_GENMSG	476
