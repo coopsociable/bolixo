bcea58cfd28d53bdc92c04a1e1b91f28f259c5ad
jack <jack@c3086684-d568-4a45-a57d-89166b08296d>
2019-03-19 13:39:47 +0000

    add_to_instrument(): New way to record in the instrument file to help understand what is going on.
    git-svn-id: https://solucorp.bolixo.org/repos/solucorp/bolixo/trunk@3397 c3086684-d568-4a45-a57d-89166b08296d

--- instrument.h
+++ instrument.h
@@ -4,6 +4,11 @@
 	#else
 		FILE *f_instrument = fopen ("/tmp/instrument.log","a");
 	#endif
+	void add_to_instrument (const char *s)
+	{
+		if (f_instrument != NULL) fprintf (f_instrument,"# %s\n",s);
+	}
 #else
 	extern FILE *f_instrument;
+	inline add_to_instrument(const char *){}
 #endif
