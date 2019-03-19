fc8cfa0bba8df5fbbb7f96575e9050ea3c30dfb3
jack <jack@c3086684-d568-4a45-a57d-89166b08296d>
2019-03-19 13:54:39 +0000

    tlmpweb_setexpires() used in file transfert. web/util.hcc now produces instrumentation.
    git-svn-id: https://solucorp.bolixo.org/repos/solucorp/bolixo/trunk@3398 c3086684-d568-4a45-a57d-89166b08296d

--- instrument.h
+++ instrument.h
@@ -1,14 +1,18 @@
 #ifdef INSTRUMENT
-	#ifdef INSTRUMENT_DONOTOPEN
-		FILE *f_instrument = NULL;
+	#ifdef INSTRUMENT_EXTERN
+		extern FILE *f_instrument;
 	#else
-		FILE *f_instrument = fopen ("/tmp/instrument.log","a");
+		#ifdef INSTRUMENT_DONOTOPEN
+			FILE *f_instrument = NULL;
+		#else
+			FILE *f_instrument = fopen ("/tmp/instrument.log","a");
+		#endif
 	#endif
-	void add_to_instrument (const char *s)
+	inline void add_to_instrument (const char *s)
 	{
 		if (f_instrument != NULL) fprintf (f_instrument,"# %s\n",s);
 	}
 #else
 	extern FILE *f_instrument;
-	inline add_to_instrument(const char *){}
+	inline void add_to_instrument(const char *){}
 #endif
