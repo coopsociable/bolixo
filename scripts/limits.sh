b2f6568d56b303c45c44a6cc09be62cdbda5385b
jack <jack@c3086684-d568-4a45-a57d-89166b08296d>
2019-03-20 19:21:14 +0000

    Small optimisation in bod web/util.hcc was closing </script> twice.
    git-svn-id: https://solucorp.bolixo.org/repos/solucorp/bolixo/trunk@3407 c3086684-d568-4a45-a57d-89166b08296d

--- scripts/limits.sh
+++ scripts/limits.sh
@@ -7,20 +7,20 @@ if [ "$1" = "" ] ; then
 	fi
 elif [ "$1" = "longurl" ] ;then # test: Create a message with a long URL
 	# bod shows NB lines by default
-	NB=5
+	NB=8
 	rm -f /tmp/msg.txt
 	for ((i=1; i<$NB; i++))
 	do
 		echo line $i: image should be visible >>/tmp/msg.txt
 	done
-	echo "line 5 is a line with more than 80 character long ending with a _IMG=http://test1.bolixo.org/talk1.jpg?agument1=0&argument2=0&argument3=0&argument4=0" >>/tmp/msg.txt
+	echo "line 8 is a line with more than 80 character long ending with a _IMG=http://test1.bolixo.org/talk1.jpg?agument1=0&argument2=0&argument3=0&argument4=0" >>/tmp/msg.txt
 	./bofs msgs -t -G public -F /tmp/msg.txt
 	rm -f /tmp/msg.txt
 	for ((i=1; i<$NB; i++))
 	do
 		echo line $i: image invisible >>/tmp/msg.txt
 	done
-	echo "line 5 is a line with more than 80 character long ending with a long URL making it invisible _IMG=http://test1.bolixo.org/talk1.jpg?agument1=0&argument2=0&argument3=0&argument4=0" >>/tmp/msg.txt
+	echo "line 8 is a line with more than 80 character long ending with a long URL making it invisible _IMG=http://test1.bolixo.org/talk1.jpg?agument1=0&argument2=0&argument3=0&argument4=0" >>/tmp/msg.txt
 	./bofs msgs -t -G public -F /tmp/msg.txt
 else
 	echo command
