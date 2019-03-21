5a2b8c42e6270ede59f452a0013fb45ada4c33e2
jack <jack@c3086684-d568-4a45-a57d-89166b08296d>
2019-03-21 03:13:40 +0000

    bo-sessiond initialize the notifies of a new login from other sessions of the same user. bo-sessiond applies delnotify to all sessions of a user. rssd --process prints the number of accounts processed. rss-scan reports the exit code of rssd so we know if it has failed. web/config.hcc had issue with the webtable cleanup. It was not using the proper WEBID names and this was confising settablerow in web/index.hcc
    git-svn-id: https://solucorp.bolixo.org/repos/solucorp/bolixo/trunk@3408 c3086684-d568-4a45-a57d-89166b08296d

--- bolixo.m
+++ bolixo.m
@@ -2,7 +2,7 @@ extern const char **_dictionary_bolixo;
 #ifndef DICTIONARY_REQUEST
 	#define DICTIONARY_REQUEST \
 	const char **_dictionary_bolixo;\
-	TRANSLATE_SYSTEM_REQ _dictionary_req_bolixo("bolixo",_dictionary_bolixo,489,15);\
+	TRANSLATE_SYSTEM_REQ _dictionary_req_bolixo("bolixo",_dictionary_bolixo,490,15);\
 	void dummy_dict_bolixo(){}
 #endif
 #ifndef MSG_U
@@ -502,3 +502,4 @@ extern const char **_dictionary_bolixo;
 #define O_PUBLISH_BOSITE	486
 #define O_KEEPMSGS	487
 #define E_FILECONTENT	488
+#define I_PROCESSED	489
