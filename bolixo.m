07b06690a12a36e9a1b965e07512e0f4e6b7ffaa
jack <jack@c3086684-d568-4a45-a57d-89166b08296d>
2019-03-22 02:02:04 +0000

    Bug with the processing of the _IMG= tag. A = was replaced by a += :-( rssd will continue its sequence even if one web site fails to return its RSS.
    git-svn-id: https://solucorp.bolixo.org/repos/solucorp/bolixo/trunk@3415 c3086684-d568-4a45-a57d-89166b08296d

--- bolixo.m
+++ bolixo.m
@@ -2,7 +2,7 @@ extern const char **_dictionary_bolixo;
 #ifndef DICTIONARY_REQUEST
 	#define DICTIONARY_REQUEST \
 	const char **_dictionary_bolixo;\
-	TRANSLATE_SYSTEM_REQ _dictionary_req_bolixo("bolixo",_dictionary_bolixo,491,15);\
+	TRANSLATE_SYSTEM_REQ _dictionary_req_bolixo("bolixo",_dictionary_bolixo,493,15);\
 	void dummy_dict_bolixo(){}
 #endif
 #ifndef MSG_U
@@ -504,3 +504,5 @@ extern const char **_dictionary_bolixo;
 #define E_FILECONTENT	488
 #define I_PROCESSED	489
 #define I_ALREADY	490
+#define E_GETRSS	491
+#define E_WEBSITENOTFOUND	492
