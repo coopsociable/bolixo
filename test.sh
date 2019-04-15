e692c22eda11d61f290997b57136cc2f00513182
jack <jack@c3086684-d568-4a45-a57d-89166b08296d>
2019-04-15 02:25:25 +0000

    Applies the marks SQL table to the talk section. Now the folders present a small icon telling if a message has been viewed or not. The marks table make this status private to each user.
    git-svn-id: https://solucorp.bolixo.org/repos/solucorp/bolixo/trunk@3499 c3086684-d568-4a45-a57d-89166b08296d

--- test.sh
+++ test.sh
@@ -1469,6 +1469,8 @@ elif [ "$1" = "lxc0-webssl" ]; then # prod:
 			-e /var/www/html/group-inbox-ui.jpg \
 			-e /var/www/html/group-inbox-ui-fr.jpg \
 			-e /var/www/html/narrowscreen.jpg \
+			-e /var/www/html/email-outline.svg \
+			-e /var/www/html/email-open-outline.svg \
 			-i /usr/sbin/trli-init \
 			-l $LOG \
 			-n $w -p /usr/sbin/httpd >/var/lib/lxc/$w/$w-lxc0.sh
