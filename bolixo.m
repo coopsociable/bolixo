e49dd45eddb41b88aef520edd8319ec9a7f01b56
jack <jack@c3086684-d568-4a45-a57d-89166b08296d>
2019-12-12 04:28:31 +0000

    Use short words to display the number of unseen messages in mobile mode.
    git-svn-id: https://solucorp.bolixo.org/repos/solucorp/bolixo/trunk@3657 c3086684-d568-4a45-a57d-89166b08296d

--- bolixo.m
+++ bolixo.m
@@ -2,7 +2,7 @@ extern const char **_dictionary_bolixo;
 #ifndef DICTIONARY_REQUEST
 	#define DICTIONARY_REQUEST \
 	const char **_dictionary_bolixo;\
-	TRANSLATE_SYSTEM_REQ _dictionary_req_bolixo("bolixo",_dictionary_bolixo,551,17);\
+	TRANSLATE_SYSTEM_REQ _dictionary_req_bolixo("bolixo",_dictionary_bolixo,552,17);\
 	void dummy_dict_bolixo(){}
 #endif
 #ifndef MSG_U
@@ -564,3 +564,4 @@ extern const char **_dictionary_bolixo;
 #define I_PRIVATE	548
 #define O_MAXUSERID	549
 #define I_LINK	550
+#define I_NEWM	551
