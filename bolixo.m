ce07de12c3be662defc4f394ab415f754638bc26
jack <jack@c3086684-d568-4a45-a57d-89166b08296d>
2020-02-29 04:08:05 +0000

    Make the bolixod search api available in bolixoapi, so exposed on the net directly.
    git-svn-id: https://solucorp.bolixo.org/repos/solucorp/bolixo/trunk@3812 c3086684-d568-4a45-a57d-89166b08296d

--- bolixo.m
+++ bolixo.m
@@ -2,7 +2,7 @@ extern const char **_dictionary_bolixo;
 #ifndef DICTIONARY_REQUEST
 	#define DICTIONARY_REQUEST \
 	const char **_dictionary_bolixo;\
-	TRANSLATE_SYSTEM_REQ _dictionary_req_bolixo("bolixo",_dictionary_bolixo,624,19);\
+	TRANSLATE_SYSTEM_REQ _dictionary_req_bolixo("bolixo",_dictionary_bolixo,625,19);\
 	void dummy_dict_bolixo(){}
 #endif
 #ifndef MSG_U
@@ -637,3 +637,4 @@ extern const char **_dictionary_bolixo;
 #define E_ONEERROR	621
 #define I_TEST	622
 #define I_DOIT	623
+#define O_USER	624
