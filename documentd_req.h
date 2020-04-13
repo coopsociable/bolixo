570fd254f9cd8b11b962cb0190858fcb72e097b4
jack <jack@c3086684-d568-4a45-a57d-89166b08296d>
2020-04-13 20:31:51 +0000

    New header for contant used to communicate between the application and documentd
    git-svn-id: https://solucorp.bolixo.org/repos/solucorp/bolixo/trunk@3923 c3086684-d568-4a45-a57d-89166b08296d

---
+++ documentd_req.h
@@ -0,0 +1,23 @@
+#ifndef DOCUMENTD_REQ_H
+#define DOCUMENTD_REQ_H
+
+// Request sent to documents
+#define REQ_PRINT	"print"
+#define REQ_GETFIELDS	"getfields"
+
+// Results produced by documents
+#define VAR_CONTENT	"content"	// HTML content to display
+#define VAR_ERROR	"error"
+#define VAR_RESULT	"result"
+#define VAR_NOTIFY	"notify"	// Javascript applied to all users
+#define VAR_REFRESH	"refresh"	// Trigger a screen refresh
+#define VAR_SCRIPT	"script"	// Javascript for this user only
+#define VAR_CHANGES	"changes"	// A change was done in the game or document worth telling everyone
+#define VAR_ENGINE	"engine"	// A message must be sent to an engine (gnuchess for now) for this game
+#define VAR_DIALOG	"dialog"	// A dialog must be presented by the UI
+#define VAR_FIELDS	"fields"	// Content for a dialog
+
+// Various per document dialog
+#define DIALOG_CHESS_CONFIG	"config"
+#define DIALOG_CHESS_NEWGAME	"newgame"
+#endif
