4cef7578e964a6b79f129314469106a3aa5376be
jack <jack@c3086684-d568-4a45-a57d-89166b08296d>
2020-04-21 05:16:14 +0000

    Documentation for the section<em>Projects</em> is more complete. The contextual menu options are explained.
    You can rename open documents and games. For the user session, this is transparent. All parts of the UI are renamed. You can rename a folder, and all documents in the hierarchy are renamed as well. If other users have one of those documents or games opened, a popup will tell them the document has been renamed. The popup identifies the user who performed the rename operations. The new name of the document is shown as well. The user just have to close the document and reopen it using the new name. All modifications are preserved.
    
    
    
    git-svn-id: https://solucorp.bolixo.org/repos/solucorp/bolixo/trunk@3945 c3086684-d568-4a45-a57d-89166b08296d

--- bolixo.m
+++ bolixo.m
@@ -2,7 +2,7 @@ extern const char **_dictionary_bolixo;
 #ifndef DICTIONARY_REQUEST
 	#define DICTIONARY_REQUEST \
 	const char **_dictionary_bolixo;\
-	TRANSLATE_SYSTEM_REQ _dictionary_req_bolixo("bolixo",_dictionary_bolixo,723,24);\
+	TRANSLATE_SYSTEM_REQ _dictionary_req_bolixo("bolixo",_dictionary_bolixo,724,24);\
 	void dummy_dict_bolixo(){}
 #endif
 #ifndef MSG_U
@@ -736,3 +736,4 @@ extern const char **_dictionary_bolixo;
 #define E_EMPTYFILE	720
 #define M_OPENINBROWSERTAB	721
 #define E_NOTAVAILDIR	722
+#define I_RENAMED	723
