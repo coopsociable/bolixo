2cd09e5b1200ba739a7cb5abc1e9db137d8ece8e
jack <jack@c3086684-d568-4a45-a57d-89166b08296d>
2020-08-18 18:13:04 +0000

    Install email notification by default for the inbox of the admin user.
    git-svn-id: https://solucorp.bolixo.org/repos/solucorp/bolixo/trunk@4074 c3086684-d568-4a45-a57d-89166b08296d

--- test.sh
+++ test.sh
@@ -880,6 +880,9 @@ elif [ "$1" = "createadmin" ] ; then # config: Create the admin account
 	# Make the admin account visible (public page)
 	echo $BOFS -u admin misc -w -V 1
 	$BOFS -u admin misc -w -V 1
+	# Enable email notificsation for the short message inbox
+	$BOFS -u admin misc --set_notification -K inbox --notify_email --notify_ui
+	# Install admin photos
 	$BOFS -u admin cp /var/www/html/admin.jpg bo://projects/admin/public/mini-photo.jpg
 	$BOFS -u admin cp /var/www/html/admin-photo.jpg bo://projects/admin/public/photo.jpg
 elif [ "$1" = "registernode" ]; then # config: Register this node in bolixo.org
