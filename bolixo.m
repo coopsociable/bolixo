fa82f4988050983a333f72d764ace3d46b794386
jack <jack@c3086684-d568-4a45-a57d-89166b08296d>
2024-03-08 18:45:13 +0000

    bofs: documents waitevent can store and retrieve the sequence number from a file. It is handy when debugging the javascript code. whiteboard: when linking documents, some elements were wrongly updated.
    git-svn-id: https://solucorp.bolixo.org/repos/solucorp/bolixo/trunk@4487 c3086684-d568-4a45-a57d-89166b08296d

--- bolixo.m
+++ bolixo.m
@@ -2,7 +2,7 @@ extern const char **_dictionary_bolixo;
 #ifndef DICTIONARY_REQUEST
 	#define DICTIONARY_REQUEST \
 	const char **_dictionary_bolixo;\
-	TRANSLATE_SYSTEM_REQ _dictionary_req_bolixo("bolixo",_dictionary_bolixo,863,25);\
+	TRANSLATE_SYSTEM_REQ _dictionary_req_bolixo("bolixo",_dictionary_bolixo,865,25);\
 	void dummy_dict_bolixo(){}
 #endif
 #ifndef MSG_U
@@ -876,3 +876,5 @@ extern const char **_dictionary_bolixo;
 #define E_APIOPER	860
 #define O_WAITONE	861
 #define E_IVLDCMDARGS	862
+#define I_SEQUENCESAVED	863
+#define O_SEQUENCEFILE	864
