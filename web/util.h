528da40410ae7ec2f553fcb801489e971fef39ac
jack <jack@c3086684-d568-4a45-a57d-89166b08296d>
2025-10-21 20:58:05 +0000

    Add an optional 2 seconds delay for the javascript function fixsize(), so we can see what it does.
    git-svn-id: https://solucorp.bolixo.org/repos/solucorp/bolixo/trunk@4715 c3086684-d568-4a45-a57d-89166b08296d

--- web/util.h
+++ web/util.h
@@ -141,7 +141,7 @@ void index_setgameid(PARAM_STRING gameid);
 void index_set_clipboard(PARAM_STRING clip);
 void util_delnotify(CONNECT_INFO &con_sess, PARAM_STRING id);
 void util_delnotify(CONNECT_INFO &con_sess, PARAM_STRING prefix, PARAM_STRING id);
-void util_endscript(PARAM_STRING urlparam);
+void util_endscript(bool fixsize_delay, PARAM_STRING urlparam);
 string util_clickable_img (PARAM_STRING url, unsigned image_width);
 void util_clickable_img (PARAM_STRING url, const char *image_width, unsigned border);
 void util_print_span(PARAM_STRING url);
